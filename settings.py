da81936a275984361d8ccef84eca53d7c38e343f
mukerejeta <62596298+mukerejeta@users.noreply.github.com>
2025-03-14 17:09:03 +0100

    Delete settings.py

--- settings.py
+++
@@ -1,5 +0,0 @@
-API_KEY = 'null'                                                         # your OpenAI API KEY
-TAVILY_KEY = 'null'                                                      # Your Tavily Search API KEY
-address = 'null'                                                         # Your Ollama address and port   
-
-
